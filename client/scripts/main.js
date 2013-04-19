bf0a05158a4b546da7dae9270cced39383cccbda
dg <dg>
2013-04-19 20:00:50 +0000

    Autodetect the WebSocket server.

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -3,7 +3,23 @@
     "use strict";
     
     window.W = {};
-    W.URL = "ws://gate.cowlark.com:8086";
+
+	/* Set this either to an explicit URL... */
+
+    //W.URL = "ws://gate.cowlark.com:8086";
+
+    /* ...or else it'll try to autodetect the URL from the current address.
+     */
+
+    if (!W.URL)
+    {
+        var loc = window.location;
+
+        W.URL =
+			((loc.protocol === "https") ? "wss://" : "ws://") +
+            loc.host +
+            ":8086";
+    }
 
     W.OnSocketOpened = function()
     {
